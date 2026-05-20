02c557bb3eb1390e33637f04d62f1a05575a2870
Namdinh16 <namdinh2k03@gmail.com>
2026-05-20 22:16:58 +0700

    add login, middleware

--- public/index.php
+++ public/index.php
@@ -1,4 +1,24 @@
+<!-- <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h2>
+        Hello World!
+    </h2>
+    <?php
+        var_dump($_GET);
+    ?>
+</body>
+</html> -->
+
 <?php
-    require_once("../app/core/app.php");
-    $app = new App()
+require_once '../app/core/App.php';
+require_once '../app/middleware.php';
+$middleware = new Middleware();
+$middleware->isAuthenticated();
+$app = new App();
 ?>
